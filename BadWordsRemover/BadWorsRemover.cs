cba09f0952ddfe3fe39a9bde33d5889ac2266ae8
BladUk <fategpan764@gmail.com>
2020-11-27 16:00:46 +0300

    First Commit

---
+++ BadWordsRemover/BadWorsRemover.cs
@@ -0,0 +1,44 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Config;
+using System;
+
+namespace BadWordsRemover
+{
+	[PluginDetails(
+		author = "[RUS]BladUk",
+		name = "BadWordsRemover",
+		description = "Removes bad words from player's nickname.",
+		id = "bladuk.badwordsremover",
+		version = "1.0.0",
+		SmodMajor = 3,
+		SmodMinor = 9,
+		SmodRevision = 7
+		)]
+	class BadWordsRemover : Plugin
+	{
+		public override void OnDisable()
+		{
+			this.Info($"Plugin {this.Details.name} has been disabled.");
+		}
+
+		public override void OnEnable()
+		{
+			this.Info($"Plugin {this.Details.name} has been enabled.");
+		}
+
+		public override void Register()
+		{
+			this.AddEventHandlers(new Events(this));
+
+			this.AddConfig(new ConfigSetting("bwr_disable", false, true, "Is BadWordsRemover plugin disabled?"));
+			this.AddConfig(new ConfigSetting("bwr_broadcast", true, true, "Whether to notify players about the word found in their nickname"));
+			this.AddConfig(new ConfigSetting("bwr_broadcast_duration", 7, true, "Duration of broadcast"));
+			this.AddConfig(new ConfigSetting("bwr_broadcast_string", "<i>A bad word has been found in your nickname.\nIt will be replaced.</i>", true, "Broadcast text"));
+			this.AddConfig(new ConfigSetting("bwr_broadcast_monospaced", true, true, "Broadcast flag"));
+			this.AddConfig(new ConfigSetting("bwr_badwords_dictionary_path", String.Empty, true, "Path to the words dictionary"));
+			this.AddConfig(new ConfigSetting("bwr_replacer_mode", "replace", true, "Replacer mode"));
+			this.AddConfig(new ConfigSetting("bwr_replacer_word", "[REDACTED]", true, "Replacer word"));
+		}
+	}
+}
---
+++ BadWordsRemover/Events.cs
@@ -0,0 +1,81 @@
+﻿using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.IO;
+
+namespace BadWordsRemover
+{
+	internal class Events : IEventHandlerWaitingForPlayers, IEventHandlerNicknameSet
+	{
+		public bool broadcast;
+		public int duration;
+		public string broadcast_string;
+		public bool isMonospaced;
+		public string path;
+		public string mode;
+		public string replace_word;
+		public string dictionarypath;
+		private readonly Plugin plugin;
+		public Events(Plugin plugin)
+		{
+			this.plugin = plugin;
+		}
+
+		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+			if (plugin.GetConfigBool("bwr_disable"))
+			{
+				plugin.PluginManager.DisablePlugin(plugin);
+			}
+			broadcast = plugin.GetConfigBool("bwr_broadcast");
+			duration = plugin.GetConfigInt("bwr_broadcast_duration");
+			broadcast_string = plugin.GetConfigString("bwr_broadcast_string");
+			isMonospaced = plugin.GetConfigBool("bwr_broadcast_monospaced");
+			mode = plugin.GetConfigString("bwr_replacer_mode");
+			replace_word = plugin.GetConfigString("bwr_replacer_word");
+			path = plugin.GetConfigString("bwr_badwords_dictionary_path");
+			dictionarypath = path + FileManager.GetPathSeparator().ToString() + "BadWordsDictionary.txt";
+			if (path.IsEmpty() || !path.IsEmpty() && !Directory.Exists(path) && !File.Exists(dictionarypath))
+			{
+				path = FileManager.GetAppFolder(true, true, "");
+				plugin.Warn("Directory doesn't exists! Please, set value for config setting bwr_badwords_dictionary_path");
+			}
+			else if (Directory.Exists(path) && !File.Exists(dictionarypath))
+			{
+				plugin.Warn($"BadWordsDictionary.txt doesn't exists! Empty file BadWordsDictionarty.txt has been created in {dictionarypath}");
+				File.Create(dictionarypath);
+			}
+		}
+		public void OnNicknameSet(PlayerNicknameSetEvent ev)
+        {
+			if (ev.Player != null)
+            {
+				foreach (string badword in File.ReadAllLines(dictionarypath))
+                {
+					if(ev.Nickname.ToLower().Contains(badword.ToLower()))
+                    {
+						switch (mode.ToLower())
+						{
+							case "replace":
+								int replaceIndex = ev.Nickname.ToLower().IndexOf(badword);
+								ev.Nickname = ev.Nickname.Remove(replaceIndex, badword.Length);
+								ev.Nickname = ev.Nickname.Insert(replaceIndex, replace_word);
+								ev.Player.PersonalClearBroadcasts();
+								ev.Player.PersonalBroadcast((uint)System.Convert.ToInt32(duration), broadcast_string, isMonospaced);
+								break;
+							case "remove":
+								int removeIndex = ev.Nickname.ToLower().IndexOf(badword);
+								ev.Nickname = ev.Nickname.Remove(removeIndex, badword.Length);
+								ev.Player.PersonalClearBroadcasts();
+								ev.Player.PersonalBroadcast((uint)System.Convert.ToInt32(duration), broadcast_string, isMonospaced);
+								break;
+							default:
+								plugin.Warn("Config value bwr_replacer_mode is set incorrectly. Use replace or remove.");
+								break;
+						}
+                    }
+                }
+            }
+        }
+	}
+}
